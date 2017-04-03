d0144287e9cd37eb24fe71eea372170edf84145d
App Academy Student <student@appacademy.io>
2017-04-03 14:02:57 -0700

    inheritance

--- employee.rb
+++ employee.rb
@@ -1,6 +1,12 @@
 class Employee
   attr_reader :name, :title, :salary, :boss
 
+  def initialize(name, salary)
+    @name = name
+    @salary = salary
+    #@boss = nil
+  end
+
   def bonus(multiplier)
     salary * multiplier
   end
@@ -9,8 +15,8 @@ end
 
 class Manager < Employee
 
-  def initialize(name)
-    @name = name
+  def initialize(name, salary)
+    super(name, salary)
     @employees = []
   end
 
@@ -18,8 +24,33 @@ class Manager < Employee
     total_employee_salary * multiplier
   end
 
+  def add_employee(name)
+    @employees << name
+    #name.boss = self
+  end
+
+  # def subordinates
+  #   @employees
+  # end
+
   def total_employee_salary
     sum = 0
-    employees.each { |employee| sum += employee.salary }
+    @employees.each do |employee|
+      if employee.is_a?(Manager)
+        sum += employee.total_employee_salary
+        sum += employee.salary
+      else
+        sum += employee.salary
+      end
+    end
+    sum
   end
 end
+
+ned = Manager.new("Ned", 1_000_000)
+darren = Manager.new("Darren", 78_000)
+shawna = Employee.new("Shawna", 12_000)
+david = Employee.new("David", 10_000)
+ned.add_employee(darren)
+darren.add_employee(shawna)
+darren.add_employee(david)
